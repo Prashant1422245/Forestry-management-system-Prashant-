f65cbe015cfa2b64317b964d0bfd88e15baa8706
Prashant1422245 <84896466+Prashant1422245@users.noreply.github.com>
2021-06-09 14:28:02 +0530

    Add files via upload

---
+++ src/main/java/com/cg/fms/ForestryManagementApplication.java
@@ -0,0 +1,14 @@
+package com.cg.fms;
+ 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class ForestryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForestryManagementApplication.class, args);
+	}
+
+}
